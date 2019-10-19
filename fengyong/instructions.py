4e1d1895cbda96ef5343632f9e14bbd92898042b
Sean Lee <lm19990323@gmail.com>
2019-10-19 21:16:57 +0800

    Added all the instructions. Those cannot be implemented currently were commented out and listed at the beginning of the file.
    The full name of each instructions is provided before the function definition.
    
    Rewrite some two-line functions so that they only take one line.

--- fengyong/instructions.py
+++ fengyong/instructions.py
@@ -3,65 +3,200 @@ from .Registers import Registers
 from .RegData import RegData
 
 
+# Currently unsupported instructions
+# Unsigned problem: 			addiu addu sltiu sltu subu multu divu
+# LO/HI problem: 				div divu mfhi mflo mult multu
+# Link problem: 				bgezal bltzal jal
+# Jump problem: 				jr
+# Byte problem: 				lb sb
+# Arithmetic shift problem: 	sra
+# Other: 						syscall
+# Oct 19, 2019
+
+
+# Add (with overflow)
+def add_(rd, rs, rt):
+    Registers.reg_set(rd, Registers.reg_get(rt) + Registers.reg_get(rs))
+
+
+# Add immediate (with overflow)
+def addi_(rt, rs, imm):
+    Registers.reg_set(rt, Registers.reg_get(rs) + int(imm))
+
+
+# Add immediate unsigned(no overflow)
+# def addiu_(rt, rs, imm):
+#     Registers.reg_set(rt, Registers.reg_get(rs) + int(imm))
+
+# Add unsigned (no overflow)
+# def addu_(rd, rs, rt):
+#     Registers.reg_set(rd, Registers.reg_get(rt) + Registers.reg_get(rs))
+
+# Bitwise and
+def and_(rd, rs, rt):
+    Registers.reg_set(rd, Registers.reg_get(rt) & Registers.reg_get(rs))
+
+
+# Bitwise and immediate
+def andi_(rt, rs, imm):
+    Registers.reg_set(rt, Registers.reg_get(rs) & int(imm))
+
+
+# Branch on equal
+def beq_(rt, rs, LABEL):
+    return LABEL if Registers.reg_get(rt) == Registers.reg_get(rs) else None
+
+
+# Branch on greater than or equal to zero
+def bgez_(rs, LABEL):
+    return LABEL if Registers.reg_get(rs) >= 0 else None
+
+
+# Branch on greater than or equal to zero and link
+# def bgezal_(rs, LABEL):
+
+# Branch on greater than zero
+def bgtz_(rs, LABEL):
+    return LABEL if Registers.reg_get(rs) > 0 else None
+
+
+# Branch on less than or equal to zero
+def blez_(rs, LABEL):
+    return LABEL if Registers.reg_get(rs) <= 0 else None
+
+
+# Branch on less than zero
+def bltz_(rs, LABEL):
+    return LABEL if Registers.reg_get(rs) < 0 else None
+
+
+# Branch on less tan zero and link
+# def bltzal_(rs, LABEL):
+
+# Divide
+# def div_(rs, rt):
+
+# Divide unsigned
+# def divu_(rs, rt):
+
+# Jump
+def j_(LABEL):
+    return LABEL
+
+
+# Jump and link
+# def jal_(LABEL):
+
+# Jump register
+# def jr_(rd):
+
+# Load Byte
+# def lb_(rt, offset, rs)
+
+# Load upper immediate
+def lui_(rt, imm):
+    Registers.reg_set(rt, RegData(int(imm < 16)))
+
+
 def lw_(rt, add, rs):
     dw = Memory.get_dw(Registers.reg_get(rs) + add)
     Registers.reg_set(rt, dw)
 
 
+# Move from HI
+# def mfhi_(rd):
+
+# Move from LO
+# def mflo_(rd):
+
+# Multiply
+# def mult_(rs, rt):
+
+# Multiply unsigned
+# def multu_(rs, rt):
+
+# No operation
+def noop_():
+    pass
+
+
+# Bitwise or
+def or_(rd, rs, rt):
+    Registers.reg_set(rd, Registers.reg_get(rt) | Registers.reg_get(rs))
+
+
+# Bitwise or immediate
+def ori_(rt, rs, imm):
+    Registers.reg_set(rt, Registers.reg_get(rs) | int(imm))
+
+
+# Store byte
+# def sb_(rt, offset, rs):
+
+# Shift left logical
+def sll_(rt, rs, imm):
+    Registers.reg_set(rt, Registers.reg_get(rs) << int(imm))
+
+
+# Shift left logical variable
+def sllv_(rd, rt, rs):
+    Registers.reg_set(rd, Registers.reg_get(rt) << Registers.reg_get(rs))
+
+
+# Set on less than (signed)
+def slt_(rd, rs, rt):
+    Registers.reg_set(rd, RegData(Registers.reg_get(rs) < Registers.reg_get(rt)))
+
+
+# Set on less than immediate (signed)
+def slti_(rt, rs, imm):
+    Registers.reg_set(rt, RegData(Registers.reg_get(rs) < int(imm)))
+
+
+# Set on less than immediate unsigned
+# def sltiu_(rt, rs, imm):
+#    Registers.reg_set(rt, RegData(Registers.reg_get(rs) < int(imm)))
+
+
+# Set on less than unsigned
+# def sltu_(rt, rs, imm):
+#    Registers.reg_set(rt, RegData(Registers.reg_get(rs) < int(imm)))
+
+# Shift right arithmetic
+# def sra_(rd, rt, shamt)
+
+# Shift right logical
+def srl_(rd, rt, shamt):
+    Registers.reg_set(rd, Registers.reg_get(rt) >> int(shamt))
+
+
+# Shift right logical variable
+def srlv_(rd, rt, rs):
+    Registers.reg_set(rd, Registers.reg_get(rt) >> Registers.reg_get(rs))
+
+
+# Subtract
+def sub_(rd, rs, rt):
+    Registers.reg_set(rd, Registers.reg_get(rt) - Registers.reg_get(rs))
+
+
+# Subtracted unsigned
+# def subu_(rd, rs, rt):
+
+# Store word
 def sw_(rt, add, rs):
     dw = Registers.reg_get(rt)
     Memory.set_dw(Registers.reg_get(rs) + add, dw)
 
 
-def add_(rd, rs, rt):
-    res = Registers.reg_get(rt) + Registers.reg_get(rs)
-    Registers.reg_set(rd, res)
+# System call
+# def syscall_():
+
+# Bitwise exclusive or
+def xor_(rd, rs, rt):
+    Registers.reg_set(rd, Registers.reg_get(rt) ^ Registers.reg_get(rs))
 
 
-def addi_(rt, rs, imme):
-    res = Registers.reg_get(rs) + int(imme)
-    Registers.reg_set(rt, res)
-
-
-def and_(rd, rs, rt):
-    res = Registers.reg_get(rt) & Registers.reg_get(rs)
-    Registers.reg_set(rd, res)
-
-
-def beq_(rt, rs, LABEL):
-    return LABEL if Registers.reg_get(rt) == Registers.reg_get(rs) else None
-
-
-def j_(LABEL):
-    return LABEL
-
-
-def or_(rd, rs, rt):
-    res = Registers.reg_get(rt) | Registers.reg_get(rs)
-    Registers.reg_set(rd, res)
-
-
-def sub_(rd, rs, rt):
-    res = Registers.reg_get(rt) - Registers.reg_get(rs)
-    Registers.reg_set(rd, res)
-
-
-def sll_(rt, rs, imme):
-    res = Registers.reg_get(rs) << int(imme)
-    Registers.reg_set(rt, res)
-
-
-def slt_(rd, rs, rt):
-    Registers.reg_set(rd, RegData(Registers.reg_get(rs) < Registers.reg_get(rt)))
-
-
-def slti_(rt, rs, imme):
-    Registers.reg_set(rt, RegData(Registers.reg_get(rs) < int(imme)))
-
-
-def srl_(rt, rs, imme):
-    res = Registers.reg_get(rs) >> int(imme)
-    Registers.reg_set(rt, res)
-
-
-# TODO: many more instructions
+# Bitwise exclusive or immediate
+def xori_(rt, rs, imm):
+    Registers.reg_set(rt, Registers.reg_get(rs) ^ int(imm))
